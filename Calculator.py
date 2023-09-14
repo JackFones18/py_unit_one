7d12a7154d891c63c92bc67cbcc624306fb5d8ca
JackFones18 <fonesja@ssfs.org>
2023-09-14 11:39:22 -0400

    idea

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,4 @@
-input(
+input("type your number here: ")
+#take user input and find the remainder.
+# if the remainder is 1, print your number is odd.
+# if the remainder is 0, print your number is even.
