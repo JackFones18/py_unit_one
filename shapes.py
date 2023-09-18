5a0d10eebc5ef7bd2d2234c1855dc2a05ac9d713
JackFones18 <fonesja@ssfs.org>
2023-09-18 15:17:34 -0400

    idea

--- shapes.py
+++ shapes.py
@@ -1,20 +1,24 @@
+
 import turtle
+turtle.speed(1000**100000)
 
-def draw_square(side_length, color):
-    turtle.color(color)
-    turtle.begin_fill()
-
-    for x in range(4):
-        turtle.forward(side_length)
-        turtle.right(90)
-    turtle.end_fill()
-
-draw_square(200, "green")
 turtle.penup()
-turtle.forward(300)
+turtle.forward(-350)
 turtle.pendown()
-draw_square(50, "gold")
+for x in range(720):
+    turtle.right(.5)
+    for x in range(4):
+        turtle.forward(200)
+        turtle.right(90)
 
+turtle.penup()
+turtle.forward(700)
+turtle.pendown()
 
+for x in range(720):
+    turtle.right(.5)
+    for x in range(4):
+        turtle.forward(200)
+        turtle.right(90)
 
 turtle.exitonclick()
---
+++ star.py
@@ -0,0 +1,11 @@
+import turtle
+
+
+for x in range(5):
+    turtle.right(144)
+    turtle.forward(300)
+
+turtle.exitonclick()
+
+
+
