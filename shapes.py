782ef9209b625de21effbfdb9ed8740b1983bb70
JackFones18 <fonesja@ssfs.org>
2023-09-20 09:01:25 -0400

    idea

--- shapes.py
+++ shapes.py
@@ -1,24 +1,17 @@
-
 import turtle
-turtle.speed(1000**100000)
 
-turtle.penup()
-turtle.forward(-350)
-turtle.pendown()
-for x in range(720):
-    turtle.right(.5)
-    for x in range(4):
-        turtle.forward(200)
-        turtle.right(90)
+turtle.speed(100)
 
-turtle.penup()
-turtle.forward(700)
-turtle.pendown()
 
-for x in range(720):
-    turtle.right(.5)
-    for x in range(4):
-        turtle.forward(200)
-        turtle.right(90)
+def draw_a_spiral(sidelength):
+    for x in range(15):
+        for x in range(3):
+            turtle.forward(sidelength)
+            turtle.left(90)
+            sidelength=sidelength+10
+    turtle.forward(sidelength)
+    turtle.left(90)
+
+draw_a_spiral(1)
 
 turtle.exitonclick()
