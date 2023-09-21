d98abe851667a17d0e92044619a4d9f8cb841806
JackFones18 <fonesja@ssfs.org>
2023-09-21 11:19:17 -0400

    finished

--- octagon_project.py
+++ octagon_project.py
@@ -1,11 +1,18 @@
 import turtle
 
 def draw_an_octogon(color,size):
-
+    turtle.begin_fill()
     for x in range(8):
         turtle.color(color)
         turtle.forward(size)
         turtle.right(45)
+    turtle.end_fill()
+    turtle.left(90)
+    turtle.penup()
+    turtle.forward(200)
+    turtle.pendown()
 
-draw_an_octogon(blue,100)
+draw_an_octogon("blue",100)
+draw_an_octogon("red", 50)
+draw_an_octogon("yellow", 200)
 turtle.exitonclick()
