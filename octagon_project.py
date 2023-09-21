e2237f267c6c731029fed5b83d91b7d69210d993
JackFones18 <fonesja@ssfs.org>
2023-09-21 11:42:53 -0400

    final changes

--- octagon_project.py
+++ octagon_project.py
@@ -1,6 +1,7 @@
 import turtle
 
-def draw_an_octogon(color,size):
+
+def draw_an_octagon(color, size):
     turtle.begin_fill()
     for x in range(8):
         turtle.color(color)
@@ -8,17 +9,18 @@ def draw_an_octogon(color,size):
         turtle.right(45)
     turtle.end_fill()
 
-def move_pen(x_coordinate,y_coordinate):
+
+def move_pen(x_coordinate, y_coordinate):
     turtle.penup()
-    turtle.goto(x_coordinate,y_coordinate)
+    turtle.goto(x_coordinate, y_coordinate)
     turtle.pendown()
 
 
-draw_an_octogon("blue",100)
-move_pen(0,250)
-draw_an_octogon("red", 100)
-move_pen(-250,250)
-draw_an_octogon("yellow", 100)
-move_pen(-250,-0)
-draw_an_octogon("pink", 100)
+draw_an_octagon("red", 100)
+move_pen(0, 250)
+draw_an_octagon("gold", 100)
+move_pen(-250, 250)
+draw_an_octagon("green", 100)
+move_pen(-250, -0)
+draw_an_octagon("blue", 100)
 turtle.exitonclick()
